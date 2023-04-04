137b5a8696b7b96807e8b19c6619ef3f0161b80b
Lucas <lucasrocini@hotmail.com>
2023-04-03 21:06:54 -0300

    Login Screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,11 @@
 import Head from 'next/head'
+import Image from 'next/image';
+import styles from '../../styles/home.module.scss';
+
+import logoImg from '../../public/logo.svg';
+
+import { Input } from '../components/ui/Input'
+import { Button } from '../components/ui/Button'
 
 export default function Home() {
   return (
@@ -6,8 +13,32 @@ export default function Home() {
     <Head>
       <title>SujeitoPizza - Faça seu login</title> 
     </Head>
-    <div>
-      <h1>Sujeito Pizzaria</h1>
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
+
+      <div className={styles.login}>
+        <form>
+          <Input
+            placeholder="Digite seu email"
+            type="text"
+          />
+
+          <Input
+            placeholder="Sua senha"
+            type="password"
+          />
+          
+          <Button
+            type="submit"
+            loading={false}
+          >
+            Acessar
+          </Button>
+        </form>
+
+        <a className={styles.text}>Nao possui uma conta? Cadastre-se</a>
+
+      </div>
     </div>
     </>
   )
